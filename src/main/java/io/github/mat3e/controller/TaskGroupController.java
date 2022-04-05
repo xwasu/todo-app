ec5239d1c29780b9cefc51fc68c002eba7874635
xwasu <paatryk.wasik@gmail.com>
2022-04-05 12:02:33 +0200

    Sekcja 12: Widok (szablony, Thymeleaf: Zadanie 12: Szablon dla grup

--- src/main/java/io/github/mat3e/controller/TaskGroupController.java
+++ src/main/java/io/github/mat3e/controller/TaskGroupController.java
@@ -4,19 +4,24 @@ import io.github.mat3e.logic.TaskGroupService;
 import io.github.mat3e.model.Task;
 import io.github.mat3e.model.TaskRepository;
 import io.github.mat3e.model.projection.GroupReadModel;
+import io.github.mat3e.model.projection.GroupTaskWriteModel;
 import io.github.mat3e.model.projection.GroupWriteModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.print.attribute.standard.Media;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/groups")
 class TaskGroupController {
     private static final Logger logger = LoggerFactory.getLogger(TaskGroupController.class);
@@ -28,23 +33,50 @@ class TaskGroupController {
         this.repository = repository;
     }
 
-    @PostMapping
+    @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
+    String showGroups(Model model) {
+        model.addAttribute("group", new GroupWriteModel());
+        return "groups";
+    }
+
+    @PostMapping(produces = MediaType.TEXT_HTML_VALUE, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    String addGroup(@ModelAttribute("group") @Valid GroupWriteModel current, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "groups";
+        }
+        service.createGroup(current);
+        model.addAttribute("group", new GroupWriteModel());
+        model.addAttribute("groups", getGroups());
+        model.addAttribute("message", "Dodano grupe!");
+        return "groups";
+    }
+
+    @PostMapping(params = "addTask", produces = MediaType.TEXT_HTML_VALUE)
+    String addGroupTask(@ModelAttribute("group") GroupWriteModel current) {
+        current.getTasks().add(new GroupTaskWriteModel());
+        return "groups";
+    }
+
+    @ResponseBody
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<GroupReadModel> createGroup(@RequestBody @Valid GroupWriteModel toCreate) {
         GroupReadModel result = service.createGroup(toCreate);
         return ResponseEntity.created(URI.create("/" + result.getId())).body(result);
     }
 
-
-    @GetMapping
+    @ResponseBody
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<List<GroupReadModel>> readAllTask() {
         return ResponseEntity.ok(service.readAll());
     }
 
-    @GetMapping("/{id}")
+    @ResponseBody
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<List<Task>> readAllTasksFromGroup(@PathVariable int id) {
         return ResponseEntity.ok(repository.findAllByGroup_Id(id));
     }
 
+    @ResponseBody
     @Transactional
     @PatchMapping("/{id}")
     public ResponseEntity<?> toggleGroup(@PathVariable int id) {
@@ -60,5 +92,10 @@ class TaskGroupController {
     @ExceptionHandler(IllegalStateException.class)
     ResponseEntity<String> handleIllegalState(IllegalStateException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ModelAttribute("groups")
+    List<GroupReadModel> getGroups() {
+        return service.readAll();
     }
 }
--- src/main/java/io/github/mat3e/model/projection/GroupTaskWriteModel.java
+++ src/main/java/io/github/mat3e/model/projection/GroupTaskWriteModel.java
@@ -2,11 +2,15 @@ package io.github.mat3e.model.projection;
 
 import io.github.mat3e.model.Task;
 import io.github.mat3e.model.TaskGroup;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 public class GroupTaskWriteModel {
+    @NotBlank(message = "Task's description must not be empty")
     private String description;
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime deadline;
 
     public String getDescription() {
