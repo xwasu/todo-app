7379e44d434a5a26ab24af5b82dc6a9d10d6e78e
xwasu <paatryk.wasik@gmail.com>
2024-05-16 19:50:49 +0200

    Spring Boot, configured Maven, Maven Wrapper and project structure with existing code

---
+++ src/test/java/io/github/xwasu/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.xwasu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
