1e2f89fcc016864c1c168eb3ae28d1aacf3844df
xwasu <paatryk.wasik@gmail.com>
2024-05-20 18:17:07 +0200

    JPA, Entity and @RepositoryRestResource

---
+++ src/main/java/io/github/xwasu/model/TaskRepository.java
@@ -0,0 +1,8 @@
+package io.github.xwasu.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+interface TaskRepository extends JpaRepository<Task, Integer> {
+}
