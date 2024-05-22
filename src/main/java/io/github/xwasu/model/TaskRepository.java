d731179de73c7aaed6f95a558ea50e360d1f7d0b
xwasu <paatryk.wasik@gmail.com>
2024-05-22 21:43:07 +0200

    @RestResource

--- src/main/java/io/github/xwasu/model/TaskRepository.java
+++ src/main/java/io/github/xwasu/model/TaskRepository.java
@@ -2,7 +2,15 @@ package io.github.xwasu.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource
 interface TaskRepository extends JpaRepository<Task, Integer> {
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Task entity);
 }
