eaf1440f89fb21a77329fc54d8ed302eb3f71c6b
xwasu <paatryk.wasik@gmail.com>
2024-05-23 15:44:20 +0200

    Spring Data findBy

--- src/main/java/io/github/xwasu/model/TaskRepository.java
+++ src/main/java/io/github/xwasu/model/TaskRepository.java
@@ -1,8 +1,11 @@
 package io.github.xwasu.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 @RepositoryRestResource
 interface TaskRepository extends JpaRepository<Task, Integer> {
@@ -13,4 +16,7 @@ interface TaskRepository extends JpaRepository<Task, Integer> {
     @Override
     @RestResource(exported = false)
     void delete(Task entity);
+
+    @RestResource(path = "done", rel = "done")
+    List<Task> findByDone(@Param("state") boolean done);
 }
