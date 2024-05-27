6befabfd454b624bf0a0bdee41d5f18fba603408
xwasu <paatryk.wasik@gmail.com>
2024-05-27 18:27:45 +0200

    ?page, ?size, ?sort, Pageable and Page

--- src/main/java/io/github/xwasu/controller/TaskController.java
+++ src/main/java/io/github/xwasu/controller/TaskController.java
@@ -1,11 +1,15 @@
 package io.github.xwasu.controller;
 
+import io.github.xwasu.model.Task;
 import io.github.xwasu.model.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @RepositoryRestController
 class TaskController {
@@ -16,9 +20,15 @@ class TaskController {
         this.repository = repository;
     }
 
-    @GetMapping("/tasks")
-    ResponseEntity<?> readAllTasks() {
+    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
+    ResponseEntity<List<Task>> readAllTasks() {
         logger.warn("Exposing all the tasks!");
         return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping(value = "/tasks")
+    ResponseEntity<List<Task>> readAllTasks(Pageable page) {
+        logger.info("Custom pageable");
+        return ResponseEntity.ok(repository.findAll(page).getContent());
     }
 }
--- src/main/java/io/github/xwasu/model/Task.java
+++ src/main/java/io/github/xwasu/model/Task.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "tasks")
-class Task {
+public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
