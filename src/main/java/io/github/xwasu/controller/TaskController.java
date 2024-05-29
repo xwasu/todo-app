51f5cc310fb8b8363f04f9135b95fa82e7ffbaa2
xwasu <paatryk.wasik@gmail.com>
2024-05-29 15:00:52 +0200

    HTTP PUT mapping

--- src/main/java/io/github/xwasu/controller/TaskController.java
+++ src/main/java/io/github/xwasu/controller/TaskController.java
@@ -2,11 +2,14 @@ package io.github.xwasu.controller;
 
 import io.github.xwasu.model.Task;
 import io.github.xwasu.model.TaskRepository;
+import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -30,5 +33,11 @@ class TaskController {
     ResponseEntity<List<Task>> readAllTasks(Pageable page) {
         logger.info("Custom pageable");
         return ResponseEntity.ok(repository.findAll(page).getContent());
+    }
+
+    @PutMapping("/tasks/{id}")
+    ResponseEntity<?> updateTask(@RequestBody @Valid Task toUpdate) {
+        repository.save(toUpdate);
+        return ResponseEntity.noContent().build();
     }
 }
