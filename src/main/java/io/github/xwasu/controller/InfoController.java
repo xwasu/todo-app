73fa5263b6e8a7ca2cac6de68e1091b9b63a5356
xwasu <paatryk.wasik@gmail.com>
2024-06-04 18:18:18 +0200

    @ConfigurationProperties and own configurations

--- src/main/java/io/github/xwasu/controller/InfoController.java
+++ src/main/java/io/github/xwasu/controller/InfoController.java
@@ -1,17 +1,19 @@
 package io.github.xwasu.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import io.github.xwasu.TaskConfigurationProperties;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class InfoController {
-    @Autowired
     private DataSourceProperties dataSource;
-    @Value("${my.prop}")
-    private String myProp;
+    private TaskConfigurationProperties myProp;
+
+    InfoController(final DataSourceProperties dataSource, final TaskConfigurationProperties myProp) {
+        this.dataSource = dataSource;
+        this.myProp = myProp;
+    }
 
     @GetMapping("/info/url")
     String url() {
@@ -19,7 +21,7 @@ class InfoController {
     }
 
     @GetMapping("/info/prop")
-    String myProp() {
-        return myProp;
+    boolean myProp() {
+        return myProp.isAllowMultipleTasksFromTemplate();
     }
 }
