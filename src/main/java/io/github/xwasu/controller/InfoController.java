dda7986ce74c905f00253791f629585139d408a3
xwasu <paatryk.wasik@gmail.com>
2024-06-04 16:16:01 +0200

    @Autowired

--- src/main/java/io/github/xwasu/controller/InfoController.java
+++ src/main/java/io/github/xwasu/controller/InfoController.java
@@ -1,19 +1,21 @@
 package io.github.xwasu.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class InfoController {
-    @Value("${spring.datasource.url}")
-    private String url;
+    @Autowired
+    private DataSourceProperties dataSource;
     @Value("${my.prop}")
     private String myProp;
 
     @GetMapping("/info/url")
     String url() {
-        return url;
+        return dataSource.getUrl();
     }
 
     @GetMapping("/info/prop")
