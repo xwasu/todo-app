9005f930f6a65550d8b31b5a3d5534d3de0c1d4d
xwasu <paatryk.wasik@gmail.com>
2024-07-10 18:45:07 +0200

    Another entity, @ManyToOne with @JoinColumn, @OneToMany and one-sided relationship

---
+++ src/main/java/io/github/xwasu/model/TaskGroup.java
@@ -0,0 +1,54 @@
+package io.github.xwasu.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "task_groups")
+public class TaskGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank(message = "Task group's description must not be empty")
+    private String description;
+    private boolean done;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "group")
+    private Set<Task> tasks;
+
+    public TaskGroup() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    void setId(final int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(final boolean done) {
+        this.done = done;
+    }
+
+    public Set<Task> getTasks() {
+        return tasks;
+    }
+
+    void setTasks(final Set<Task> tasks) {
+        this.tasks = tasks;
+    }
+}
