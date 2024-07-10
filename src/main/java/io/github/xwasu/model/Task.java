516ae4b7d14c9822e40cd2641558471e242031f8
xwasu <paatryk.wasik@gmail.com>
2024-07-10 16:36:26 +0200

    Inheritance

--- src/main/java/io/github/xwasu/model/Task.java
+++ src/main/java/io/github/xwasu/model/Task.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "tasks")
-public class Task {
+public class Task extends BaseAuditableEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
@@ -15,8 +15,6 @@ public class Task {
     private String description;
     private boolean done;
     private LocalDateTime deadline;
-    private LocalDateTime createdOn;
-    private LocalDateTime updatedOn;
 
     public Task() {
     }
@@ -57,15 +55,5 @@ public class Task {
         description = source.description;
         done = source.done;
         deadline = source.deadline;
-    }
-
-    @PrePersist
-    void prePersist() {
-        createdOn = LocalDateTime.now();
-    }
-
-    @PreUpdate
-    void preMerge() {
-        updatedOn = LocalDateTime.now();
     }
 }
