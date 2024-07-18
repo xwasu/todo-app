76c15a5b99f2a1074be355a9795f5a495f618fc5
xwasu <paatryk.wasik@gmail.com>
2024-07-18 14:16:41 +0200

    Project Service

---
+++ src/main/java/io/github/xwasu/logic/ProjectService.java
@@ -0,0 +1,51 @@
+package io.github.xwasu.logic;
+
+import io.github.xwasu.TaskConfigurationProperties;
+import io.github.xwasu.model.*;
+import io.github.xwasu.model.projection.GroupReadModel;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ProjectService {
+    private ProjectRepository repository;
+    private TaskGroupRepository taskGroupRepository;
+    private TaskConfigurationProperties config;
+
+    public ProjectService(final ProjectRepository repository, final TaskGroupRepository taskGroupRepository, final TaskConfigurationProperties config) {
+        this.repository = repository;
+        this.taskGroupRepository = taskGroupRepository;
+        this.config = config;
+    }
+
+    public List<Project> readAll() {
+        return repository.findAll();
+    }
+
+    public Project save(final Project toSave) {
+        return repository.save(toSave);
+    }
+
+    public GroupReadModel createGroup(LocalDateTime deadline, int projectId) {
+        if (!config.getTemplate().isAllowMultipleTasks() && taskGroupRepository.existsByDoneIsFalseAndProject_Id(projectId)) {
+            throw new IllegalStateException("Only one undone group from project is allowed");
+        }
+        TaskGroup result = repository.findById(projectId)
+                .map(project -> {
+                    var targetGroup = new TaskGroup();
+                    targetGroup.setDescription(project.getDescription());
+                    targetGroup.setTasks(
+                            project.getSteps().stream()
+                                    .map(projectStep -> new Task(
+                                            projectStep.getDescription(),
+                                            deadline.plusDays(projectStep.getDaysToDeadline()))
+                                    ).collect(Collectors.toSet())
+                    );
+                    return targetGroup;
+                }).orElseThrow(() -> new IllegalArgumentException("Project with given id not found"));
+        return new GroupReadModel(result);
+    }
+}
