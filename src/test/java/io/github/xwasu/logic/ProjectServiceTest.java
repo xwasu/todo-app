4deef00317cac7ccba8bb3128570f2cfc7168fb9
xwasu <paatryk.wasik@gmail.com>
2024-07-19 15:15:12 +0200

    Unit tests

---
+++ src/test/java/io/github/xwasu/logic/ProjectServiceTest.java
@@ -0,0 +1,12 @@
+package io.github.xwasu.logic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectServiceTest {
+
+    @Test
+    void createGroup() {
+    }
+}
