81284efa4c305f3125f4678b96f9f02f3ee47f09
xwasu <paatryk.wasik@gmail.com>
2024-07-19 15:32:29 +0200

    JUnit

--- src/test/java/io/github/xwasu/logic/ProjectServiceTest.java
+++ src/test/java/io/github/xwasu/logic/ProjectServiceTest.java
@@ -1,5 +1,6 @@
 package io.github.xwasu.logic;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,6 +8,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class ProjectServiceTest {
 
     @Test
-    void createGroup() {
+    @DisplayName("should throw IllegalStateException when configured to allow just 1 group and the other undone group exists")
+    void createGroup_noMultipleGroupsConfig_And_undoneGroupExists_throwsIllegalStateException() {
+        // given
+        // when
+        // then
     }
 }
