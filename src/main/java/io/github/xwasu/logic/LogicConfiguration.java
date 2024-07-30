ba31d3cad14e44501003c0b57d158fcce0b1c303
xwasu <paatryk.wasik@gmail.com>
2024-07-30 21:27:37 +0200

    XML

--- src/main/java/io/github/xwasu/logic/LogicConfiguration.java
+++ src/main/java/io/github/xwasu/logic/LogicConfiguration.java
@@ -5,15 +5,17 @@ import io.github.xwasu.model.ProjectRepository;
 import io.github.xwasu.model.TaskGroupRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
+@ImportResource("classpath:applicationContext.xml")
 class LogicConfiguration {
-    @Bean
+    /*@Bean
     ProjectService service(
             final ProjectRepository repository,
             final TaskGroupRepository taskGroupRepository,
             final TaskConfigurationProperties config
     ) {
         return new ProjectService(repository, taskGroupRepository, config);
-    }
+    }*/
 }
