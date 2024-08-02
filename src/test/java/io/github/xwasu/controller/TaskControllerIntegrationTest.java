85657dfdf14ebe49c3373d11ec27b6b1d6d7e736
xwasu <paatryk.wasik@gmail.com>
2024-08-02 14:43:51 +0200

    Integration test: @AutoConfigureMockMvc and MockMvc

---
+++ src/test/java/io/github/xwasu/controller/TaskControllerIntegrationTest.java
@@ -0,0 +1,36 @@
+package io.github.xwasu.controller;
+
+import io.github.xwasu.model.Task;
+import io.github.xwasu.model.TaskRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("integration")
+class TaskControllerIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private TaskRepository repo;
+
+    @Test
+    void httpGet_returnsGivenTask() throws Exception {
+        // given
+        int id = repo.save(new Task("foo", LocalDateTime.now())).getId();
+
+        // when + then
+        mockMvc.perform(get("/tasks/" + id))
+                .andExpect(status().is2xxSuccessful());
+    }
+}
