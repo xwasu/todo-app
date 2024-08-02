97bb4017cda9b2f05c032fb59587695afd3b0f1d
xwasu <paatryk.wasik@gmail.com>
2024-08-02 16:20:03 +0200

    @WebMvcTest, @MockBean

---
+++ src/test/java/io/github/xwasu/controller/TaskControllerLightIntegrationTest.java
@@ -0,0 +1,41 @@
+package io.github.xwasu.controller;
+
+import io.github.xwasu.model.Task;
+import io.github.xwasu.model.TaskRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@WebMvcTest(TaskController.class)
+class TaskControllerLightIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private TaskRepository repo;
+
+    @Test
+    void httpGet_returnsGivenTask() throws Exception {
+        // given
+        String description = "foo";
+        when(repo.findById(anyInt()))
+                .thenReturn(Optional.of(new Task(description, LocalDateTime.now())));
+
+        // when + then
+        mockMvc.perform(get("/tasks/123"))
+                .andDo(print())
+                .andExpect(content().string(containsString(description)));
+    }
+}
