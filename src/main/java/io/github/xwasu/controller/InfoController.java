2ccc904ed47437b00f8fa9b32128674c91bf83d7
xwasu <paatryk.wasik@gmail.com>
2024-09-11 22:24:06 +0200

    Common part in the InfoController class

--- src/main/java/io/github/xwasu/controller/InfoController.java
+++ src/main/java/io/github/xwasu/controller/InfoController.java
@@ -3,9 +3,11 @@ package io.github.xwasu.controller;
 import io.github.xwasu.TaskConfigurationProperties;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/info")
 class InfoController {
     private DataSourceProperties dataSource;
     private TaskConfigurationProperties myProp;
@@ -15,12 +17,12 @@ class InfoController {
         this.myProp = myProp;
     }
 
-    @GetMapping("/info/url")
+    @GetMapping("/url")
     String url() {
         return dataSource.getUrl();
     }
 
-    @GetMapping("/info/prop")
+    @GetMapping("/prop")
     boolean myProp() {
         return myProp.getTemplate().isAllowMultipleTasks();
     }
