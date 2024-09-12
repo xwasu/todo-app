6f48f363e2d5300739ed377d0e5d4744b005a1a7
xwasu <paatryk.wasik@gmail.com>
2024-09-12 21:20:32 +0200

    Injecting all interceptors (collection injection)

--- src/main/java/io/github/xwasu/controller/MvcConfiguration.java
+++ src/main/java/io/github/xwasu/controller/MvcConfiguration.java
@@ -1,13 +1,22 @@
 package io.github.xwasu.controller;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Set;
+
 @Configuration
 class MvcConfiguration implements WebMvcConfigurer {
+    private Set<HandlerInterceptor> interceptors;
+
+    MvcConfiguration(final Set<HandlerInterceptor> interceptors) {
+        this.interceptors = interceptors;
+    }
+
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
-        registry.addInterceptor(new LoggerInterceptor());
+        interceptors.forEach(registry::addInterceptor);
     }
 }
