b96093e4dcd0bd61acdac943d7b07a441bb3a9fe
xwasu <paatryk.wasik@gmail.com>
2024-09-12 20:56:31 +0200

    Interceptor

---
+++ src/main/java/io/github/xwasu/controller/LoggerInterceptor.java
@@ -0,0 +1,17 @@
+package io.github.xwasu.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+class LoggerInterceptor implements HandlerInterceptor {
+    private static final Logger logger = LoggerFactory.getLogger(LoggerInterceptor.class);
+
+    @Override
+    public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler) throws Exception {
+        logger.info("[doFilter] " + request.getMethod() + " " + request.getRequestURI());
+        return true;
+    }
+}
