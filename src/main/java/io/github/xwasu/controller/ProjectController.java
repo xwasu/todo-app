4cd679b7ab8474d88587021da342ff70788c2818
xwasu <paatryk.wasik@gmail.com>
2024-09-19 16:28:05 +0200

    Template and controller connection

---
+++ src/main/java/io/github/xwasu/controller/ProjectController.java
@@ -0,0 +1,14 @@
+package io.github.xwasu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/projects")
+class ProjectController {
+    @GetMapping
+    String showProjects() {
+        return "projects";
+    }
+}
