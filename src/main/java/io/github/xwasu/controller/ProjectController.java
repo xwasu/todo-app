855f9028bd88032a8a15c38d2282a345198f97fb
xwasu <paatryk.wasik@gmail.com>
2024-09-20 11:28:21 +0200

    Nested objects in the form

--- src/main/java/io/github/xwasu/controller/ProjectController.java
+++ src/main/java/io/github/xwasu/controller/ProjectController.java
@@ -11,9 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 class ProjectController {
     @GetMapping
     String showProjects(Model model) {
-        var projectToEdit = new ProjectWriteModel();
-        projectToEdit.setDescription("test");
-        model.addAttribute("project", projectToEdit);
+        model.addAttribute("project", new ProjectWriteModel());
         return "projects";
     }
 }
