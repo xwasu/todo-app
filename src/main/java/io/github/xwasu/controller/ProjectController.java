5c03fe17c33871eb5ebd777a3f58bd8476fe01a5
xwasu <paatryk.wasik@gmail.com>
2024-09-23 22:42:05 +0200

    Recording via form, th:if

--- src/main/java/io/github/xwasu/controller/ProjectController.java
+++ src/main/java/io/github/xwasu/controller/ProjectController.java
@@ -1,5 +1,6 @@
 package io.github.xwasu.controller;
 
+import io.github.xwasu.logic.ProjectService;
 import io.github.xwasu.model.ProjectStep;
 import io.github.xwasu.model.projection.ProjectWriteModel;
 import org.springframework.stereotype.Controller;
@@ -12,9 +13,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/projects")
 class ProjectController {
+    private final ProjectService service;
+
+    ProjectController(final ProjectService service) {
+        this.service = service;
+    }
+
     @GetMapping
     String showProjects(Model model) {
         model.addAttribute("project", new ProjectWriteModel());
+        return "projects";
+    }
+
+    @PostMapping
+    String addProject(@ModelAttribute("project") ProjectWriteModel current, Model model) {
+        service.save(current);
+        model.addAttribute("project", new ProjectWriteModel());
+        model.addAttribute("message", "Dodano projekt!");
         return "projects";
     }
 
