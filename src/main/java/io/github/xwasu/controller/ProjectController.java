55736e8ecc9d7e3efedad1523eaee1826db6ab8a
xwasu <paatryk.wasik@gmail.com>
2024-09-25 18:44:48 +0200

    Validation in views

--- src/main/java/io/github/xwasu/controller/ProjectController.java
+++ src/main/java/io/github/xwasu/controller/ProjectController.java
@@ -4,8 +4,10 @@ import io.github.xwasu.logic.ProjectService;
 import io.github.xwasu.model.Project;
 import io.github.xwasu.model.ProjectStep;
 import io.github.xwasu.model.projection.ProjectWriteModel;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,7 +31,13 @@ class ProjectController {
     }
 
     @PostMapping
-    String addProject(@ModelAttribute("project") ProjectWriteModel current, Model model) {
+    String addProject(@ModelAttribute("project") @Valid ProjectWriteModel current,
+                      BindingResult bindingResult,
+                      Model model
+    ) {
+        if (bindingResult.hasErrors()) {
+            return "projects";
+        }
         service.save(current);
         model.addAttribute("project", new ProjectWriteModel());
         model.addAttribute("message", "Dodano projekt!");
